b9adc0afc75e1053f531dbe5e2d0d3b198d9534a
Fapret <78763775+fapret@users.noreply.github.com>
2023-11-28 06:51:29 -0300

    agregados include en mesh.h
    debido a que no reconocia algunas funciones de opengl

--- OpenGL-basico/Mesh.h
+++ OpenGL-basico/Mesh.h
@@ -1,10 +1,17 @@
 #pragma once
+#include <glad/glad.h>
+#include "SDL.h"
+#include "SDL_opengl.h"
+#include "FreeImage.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <assimp/Importer.hpp>      // C++ importer interface
 #include <assimp/scene.h>           // Output data structure
 #include <assimp/postprocess.h>     // Post processing flags
 #include "Vector3.h"
-#include <SDL.h>
-#include <SDL_opengl.h>
 
 Vector3** DoTheImportThing(const std::string& pFile, int& faceAmount);
 
