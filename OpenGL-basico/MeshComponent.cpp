581096aa39cfebdc65b38e770efa0dcdb1dcd0e2
Fapret <78763775+fapret@users.noreply.github.com>
2023-11-30 16:14:55 -0300

    meshcomponent

--- OpenGL-basico/MeshComponent.cpp
+++ OpenGL-basico/MeshComponent.cpp
@@ -1,11 +1,78 @@
 #include "MeshComponent.h"
 
+float MeshComponent::calculateDistance(const glm::vec3& point1, const glm::vec3& point2)
+{
+	float distance = glm::sqrt(glm::pow(point2.x - point1.x, 2) +
+		glm::pow(point2.y - point1.y, 2) +
+		glm::pow(point2.z - point1.z, 2));
+	return distance;
+}
+
 MeshComponent::MeshComponent() : EntityComponent("MeshComponent")
 {
+	this->maxViewDistance = 10000.0f;
+	this->transform = new TransformComponent();
+}
+
+MeshComponent::MeshComponent(float maxDistance) : EntityComponent("MeshComponent")
+{
+	this->maxViewDistance = maxDistance;
+	this->transform = new TransformComponent();
 }
 
 void MeshComponent::draw()
 {
+	Hierarchy& hierarchy = Hierarchy::getInstance();
+	Entity* camEntity = hierarchy.getActiveCamera();
+	if (camEntity != nullptr) {
+		std::vector<EntityComponent*> camTransformList = camEntity->findComponentsByType("TransformComponent");
+		if (camTransformList.empty()) {
+			return;
+		}
+		TransformComponent* camTransform = (TransformComponent*)camTransformList.at(0);
+		float distance = calculateDistance(camTransform->getPosition(), transform->getPosition());
+
+		// Find the appropriate LOD level based on distance
+		LOD* selectedLOD = nullptr;
+		float minDistanceDifference = std::numeric_limits<float>::max();
+
+		for (auto& lod : LodLevels) {
+			float distanceDifference = std::abs(distance - lod.viewDistance);
+			if (distanceDifference < minDistanceDifference) {
+				minDistanceDifference = distanceDifference;
+				selectedLOD = &lod;
+			}
+		}
+
+		if (selectedLOD) {
+			RenderMultipleMeshVAO(selectedLOD->vao, selectedLOD->faceAmount);
+		}
+
+	}
+}
+
+void MeshComponent::setMaxViewDistance(float maxView)
+{
+	this->maxViewDistance = maxView;
+}
+
+void MeshComponent::addLOD(LOD lod)
+{
+	this->LodLevels.push_back(lod);
+}
+
+int MeshComponent::getMaxViewDistance()
+{
+	return maxViewDistance;
+}
+
+void MeshComponent::setFatherEntity(Entity* father)
+{
+	EntityComponent::setFatherEntity(father);
+	std::vector<EntityComponent*> transformList = father->findComponentsByType("TransformComponent");
+	if (transformList.size() == 0) {
+		father->addComponent(transform);
+	}
 }
 
 #ifdef USE_IMGUI
--- OpenGL-basico/MeshComponent.h
+++ OpenGL-basico/MeshComponent.h
@@ -1,10 +1,32 @@
 #pragma once
 #include "EntityComponent.h"
+#include <vector>
+#include "Mesh.h"
+#include "TransformComponent.h"
+#include "Hierarchy.h"
+#include "CameraComponent.h"
+
+struct LOD {
+	std::vector<GLuint> vao;
+	std::vector<MeshData> meshData;
+	float viewDistance;
+	std::vector<int> faceAmount;
+};
 
 class MeshComponent : public EntityComponent {
+private:
+	float maxViewDistance;
+	std::vector<LOD> LodLevels;
+	TransformComponent* transform;
+	float calculateDistance(const glm::vec3& point1, const glm::vec3& point2);
 public:
 	MeshComponent();
+	MeshComponent(float maxDistance);
 	void draw() override;
+	void setMaxViewDistance(float maxView);
+	void addLOD(LOD lod);
+	int getMaxViewDistance();
+	void setFatherEntity(Entity* father) override;
 #ifdef USE_IMGUI
 	void EditorPropertyDraw() override;
 #endif
